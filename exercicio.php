2b207c37d49bc997831c5f95e1e665763d525e5b
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-02-09 17:37:15 -0300

    Adicionado pagina de exercicios

---
+++ exercicio.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:400,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/exercicio.css" />
+    <title>Exercício</title>
+</head>
+<body class="exercicio">
+ 
+    <header class="header">  
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício.</h2>
+    </header>
+
+      <nav class="navigation">
+        <a href="#" class="green">Sem formatação</a>
+        <a href="index.php" class="red">Voltar</a>
+      </nav>
+    
+    <main class="main">
+        <div class="content">
+            
+
+        </div>
+    </main>
+
+
+
+    <footer class="footer">
+        Copyright © <?= date('Y'); ?> | Pedro Giampietro
+    </footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:400,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <title>Curso PHP.</title>
+</head>
+<body>
+ 
+    <header class="header">  
+        <h1>Projeto - Curso PHP!</h1>
+        <h2>ìndices</h2>
+    </header>
+      
+    
+    <main class="main">
+        <div class="content">
+            <nav class="modules">
+                <div class="module green">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li><a href="exercicio.php">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li> <a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+
+                <div class="module red">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li>
+                            <li><a href="#">Exercicio A</a></li>
+                            <li><a href="#">Exercicio B</a></li>
+                            <li> <a href="#">Exercicio C</a></li>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="module blue">
+                    <h3>Módulo 03</h3>
+                    <ul>
+                        <li>
+                            <li><a href="#">Exercicio A</a></li>
+                            <li><a href="#">Exercicio B</a></li>
+                            <li> <a href="#">Exercicio C</a></li>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="module purple">
+                    <h3>Módulo 04</h3>
+                    <ul>
+                        <li>
+                            <li><a href="#">Exercicio A</a></li>
+                            <li><a href="#">Exercicio B</a></li>
+                            <li> <a href="#">Exercicio C</a></li>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="module orange">
+                    <h3>Módulo 05</h3>
+                    <ul>
+                        <li>
+                            <li><a href="#">Exercicio A</a></li>
+                            <li><a href="#">Exercicio B</a></li>
+                            <li> <a href="#">Exercicio C</a></li>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="module dark-green">
+                    <h3>Módulo 06</h3>
+                    <ul>
+                        <li>
+                            <li><a href="#">Exercicio A</a></li>
+                            <li><a href="#">Exercicio B</a></li>
+                            <li> <a href="#">Exercicio C</a></li>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="module dark-red">
+                    <h3>Módulo 07</h3>
+                    <ul>
+                        <li>
+                            <li><a href="#">Exercicio A</a></li>
+                            <li><a href="#">Exercicio B</a></li>
+                            <li> <a href="#">Exercicio C</a></li>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="module dark-blue">
+                    <h3>Módulo 08</h3>
+                    <ul>
+                        <li>
+                            <li><a href="#">Exercicio A</a></li>
+                            <li><a href="#">Exercicio B</a></li>
+                            <li> <a href="#">Exercicio C</a></li>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="module dark-purple">
+                    <h3>Módulo 09</h3>
+                    <ul>
+                        <li>
+                            <li><a href="#">Exercicio A</a></li>
+                            <li><a href="#">Exercicio B</a></li>
+                            <li> <a href="#">Exercicio C</a></li>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+
+
+
+    <footer class="footer">
+        Copyright © <?= date('Y'); ?> | Pedro Giampietro
+    </footer>
+
+</body>
+</html>
